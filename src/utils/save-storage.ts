b849c75cb881dea045aaa18a98c9b8fcd3bf56c2
NickieDev <163011425+NickieDev@users.noreply.github.com>
2025-08-27 14:57:08 -0300

    Update save-storage.ts

--- src/utils/save-storage.ts
+++ src/utils/save-storage.ts
@@ -1,3 +1,5 @@
-export const saveStorage = async (key: string, storage: string | number | boolean) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const saveStorage = async (key: string, storage: any) => {
    return localStorage.setItem(key, JSON.stringify(storage))
+
 }
